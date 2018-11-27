4e6b3ad17b4e484da3c15d75c84a54c09d9a09a9
Glenn LeBlanc <gl3nnleblanc@berkeley.edu>
2018-11-26 17:25:35 -0800

    Working implementation; kept fft2test for reference.

---
+++ fft2d.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from mpl_toolkits.mplot3d import Axes3D
+# Import square image with appropriate filepath for aperture. white-> opening; black-> blockage
+img_path='./aperture5.png'
+img=cv2.imread(img_path,0).astype(float)*.001
+res=len(img)
+
+# Do fft on image with numpy
+img_fft=np.abs(np.fft.fftshift(np.fft.fft2(img))).transpose()
+
+plt.imshow(img_fft)
+plt.show()
