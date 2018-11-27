a8f4ecba6b5cd04024e077ec37aaccd1b640c546
Glenn LeBlanc <g.leblanc135@gmail.com>
2018-11-26 18:14:02 -0800

    Delete fraunhofer.py

--- fraunhofer.py
+++
@@ -1,7 +0,0 @@
-# Preliminary - copied from stackoverflow (Plot the 2D FFT of an image)
-from scipy import fftpack, ndimage
-import matplotlib.pyplot as plt
-image = plt.imread('rect2.png')
-fft2 = fftpack.fft2(image)
-plt.imshow(fft2.imag)
-plt.show()
